fe0e0d2d2bb4e50475eceb88016708df4b9cdd82
Flor <florenciaz@gmail.com>
2017-12-25 13:08:40 -0300

    Modificaciones para incorporar rutas y vistas con pug.

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,11 +4,30 @@ var VoteHandler = require(process.cwd() + '/app/controllers/voteHandler.server.j
 
 module.exports = function (app, db) {
 
+  //load view engine
+  app.set('views', process.cwd() + '/views');
+  app.set('view engine', 'pug');
+  
 	var voteHandler = new VoteHandler(db);
 
 	app.route('/').get(function (req, res) {
-		res.sendFile(process.cwd() + '/public/index.html');
+		res.render('index', {title: 'Encuestas', encuestas: []});
 	});
+  
+  app.route('/usuarios').get(function(req, res) {
+    res.render('usuarios', {title: 'Usuarios', usuarios: []});
+  });
+  
+  app.route('/login')
+    .get(function(req, res) {
+      res.render('login', {title: 'Log in'});
+  })
+    .post(function (req, res) {
+      Usuario.findOne({ id: req.body.id}, function (err, usuario) {
+			if (err) { throw err;}
+			res.render('usuario', {usuario: usuario});
+		});
+  });
 
 	app.route('/api/clicks1')
 						.get(voteHandler.getClicks)
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express'),
 	routes = require('./app/routes/index.js'),
 	mongo = require('mongodb').MongoClient;
 
+const bp = require('body-parser');
+
 var app = express();
 
 mongo.connect('mongodb://localhost:27017/vota', function(err, db) {
@@ -13,10 +15,15 @@ mongo.connect('mongodb://localhost:27017/vota', function(err, db) {
 		console.log('MongoDB successfully connected on port 27017.');
 	}
 
+//body parser middleware
+app.use(bp.urlencoded({extended: false}));
+app.use(bp.json());
 
 	app.use('/public', express.static(process.cwd() + '/public'));
 	app.use('/controllers', express.static(process.cwd() +'/app/controllers'));
 
+  
+  
 	routes(app, db);
 
 	app.listen(4000, function () {
