78eadea2c780137b0477615ada001f94000b6435
Flor <florenciaz@gmail.com>
2018-01-19 17:43:17 -0300

    app.js: Pulí algunos comentarios.

--- app.js
+++ app.js
@@ -34,31 +34,16 @@ promise.then(function(db){
 	console.log('Connected to mongodb');
 });
 
-
-
-//Esta línea la copié del tutorial https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/Tutorial_local_library_website
-//Después de que obtuviera el error:
+//error:
 /*
 $ node app
 Server started on port 8080...
 (node:9479) UnhandledPromiseRejectionWarning: MongoError: failed to connect to server [localhost:27017] on first connect [MongoError: connect ECONNREFUSED 127.0.0.1:27017]
-    at Pool.<anonymous> (/home/flor/fcc/VOTAPP/votapp-pug/node_modules/mongodb-core/lib/topologies/server.js:336:35)
-    at Pool.emit (events.js:159:13)
-    at Connection.<anonymous> (/home/flor/fcc/VOTAPP/votapp-pug/node_modules/mongodb-core/lib/connection/pool.js:280:12)
-    at Object.onceWrapper (events.js:254:19)
-    at Connection.emit (events.js:159:13)
-    at Socket.<anonymous> (/home/flor/fcc/VOTAPP/votapp-pug/node_modules/mongodb-core/lib/connection/connection.js:187:49)
-    at Object.onceWrapper (events.js:254:19)
-    at Socket.emit (events.js:159:13)
-    at emitErrorNT (internal/streams/destroy.js:64:8)
-    at process._tickCallback (internal/process/next_tick.js:152:19)
-(node:9479) UnhandledPromiseRejectionWarning: Unhandled promise rejection. This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). (rejection id: 1)
-(node:9479) [DEP0018] DeprecationWarning: Unhandled promise rejections are deprecated. In the future, promise rejections that are not handled will terminate the Node.js process with a non-zero exit code.
-
 */
-//db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.on('error', function(err) {throw err;});
 
+//Solución:
+//https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/Tutorial_local_library_website
+db.on('error', function(err) {throw err;});
 
 
 //init app
@@ -98,8 +83,6 @@ app.use(function (req, res, next) {
   next();
 });
 
-// Validator middleware
-
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -120,15 +103,7 @@ app.use(function(req, res, next){
 //manejador de todo tipo de errores que  pueden suceder llegado este punto.
 app.use(function(err, req, res, next){
   res.status(err.status || 500);
-
-  //res.json({
-  //  error: {
-  //    message: err.message
-  //  }
-  //});
-
   res.render('error', {error: err});
-
 });
 
 //Start server
