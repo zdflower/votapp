c4f813283f1d4f8f19071309e7bb0a2e00e448ca
Flor <florenciaz@gmail.com>
2018-01-22 18:47:08 -0300

    controller_encuestas.js: Corrección de espacios.

--- controllers/controller_encuestas.js
+++ controllers/controller_encuestas.js
@@ -5,13 +5,13 @@ exports.obtenerEncuestas = function (req, res, next){
     if (err){
       return next(err);
     } else {
-      //no puedo acceder a req.user.username si no hay ningún usuario logueado.
+      // no puedo acceder a req.user.username si no hay ningún usuario logueado.
       if (req.user){
-        //paso el usuario porque lo usa layout
-        res.render('index', { title: 'Encuestas', encuestas: encuestas, usuario: req.user});
+        // paso el usuario porque lo usa layout
+        res.render('index', {title: 'Encuestas', encuestas: encuestas, usuario: req.user});
       } else {
-        //acá si no hay un usuario logueado no va a hacer falta pasar usuario
-        res.render('index', { title: 'Encuestas', encuestas: encuestas});
+        // acá si no hay un usuario logueado no va a hacer falta pasar usuario
+        res.render('index', {title: 'Encuestas', encuestas: encuestas});
       }
     }
   });
@@ -20,26 +20,26 @@ exports.obtenerEncuestas = function (req, res, next){
 exports.obtenerEncuesta = function(req, res, next) {
   Encuesta.findOne({pregunta: req.params.pregunta, creador: req.params.username}, function(err, encuesta) {
     if (err) {
-      //console.log(err);
-      //res.send(err);
+      // console.log(err);
+      // res.send(err);
       return next(err);
     } else {
-        if (encuesta){
-          //console.log('FINDONE: ' + encuesta);
-          //si hay un usuario logueado pasar el nombre
-          if (req.user){
-            res.render('encuesta', {title: 'Encuesta',encuesta: encuesta, usuario: req.user});
-          } else {
-            res.render('encuesta', {title: 'Encuesta',encuesta: encuesta});
-          }
+      if (encuesta){
+        // console.log('FINDONE: ' + encuesta);
+        // si hay un usuario logueado pasar el nombre
+        if (req.user){
+          res.render('encuesta', {title: 'Encuesta', encuesta: encuesta, usuario: req.user});
         } else {
-          //res.send('No existe la encuesta');
-          const error = new Error('No existe la encuesta');
-          error.status = 404;
-          next(error);
+          res.render('encuesta', {title: 'Encuesta', encuesta: encuesta});
         }
+      } else {
+        // res.send('No existe la encuesta');
+        const error = new Error('No existe la encuesta');
+        error.status = 404;
+        next(error);
+      }
     }
- });
+  });
 };
 
 exports.crearEncuesta_get = function(req, res){
@@ -60,15 +60,15 @@ exports.crearEncuesta_post = function(req, res, next) {
     let nueva_encuesta = new Encuesta(nuevaEncuesta(req.body));
     // Antes de esto hay que asegurarse que está todo ok con la creación de la encuesta.
     nueva_encuesta.creador = usuario_logueado.local.username;
-    nueva_encuesta.save( function (err) {
+    nueva_encuesta.save(function (err) {
       console.log("Guardando encuesta en la base de datos de encuestas.")
       if (err) {
-        //res.send(err);
+        // res.send(err);
         req.flash('error', 'Algo salió mal al intentar guardar la encuesta.');
         res.redirect(usuario_logueado.url + '/crearEncuesta');
       } else {
         req.flash('success', 'Encuesta creada');
-        //res.redirect(nueva_encuesta.url);
+        // res.redirect(nueva_encuesta.url);
         res.send('Ok');
       }
     });
@@ -83,61 +83,60 @@ exports.votarEncuesta = function(req, res, next) {
   Encuesta.findOne(filtro).exec(function(err, encuesta){
     if (err) {
       return next(err);
-    }
-    else {
+    } else {
       let opt = req.body.op;
-      console.log("Opciones:" + encuesta.opciones );
-      //no puedo usar directamente indexOf porque opciones es una lista de {op: ..., votos: ...}
+      console.log("Opciones:" + encuesta.opciones);
+      // no puedo usar directamente indexOf porque opciones es una lista de {op: ..., votos: ...}
       let i = indiceDe(opt, encuesta.opciones);
       console.log("índice de " + opt + " (la opción votada): " + i);
-      if (i != -1){
-        let query = 'opciones.' + i +'.votos';
+      if (i !== -1){
+        let query = 'opciones.' + i + '.votos';
         let obj = {[query] : 1};
-        Encuesta.findOneAndUpdate(filtro,	{ $inc: obj }, {new: true}).exec(function(err, enc) {
+        Encuesta.findOneAndUpdate(filtro, { $inc: obj }, {new: true}).exec(function(err, enc) {
           if (err) {
-             return next(err);
+            return next(err);
           } else {
             req.flash('success', 'Voto registrado.');
             res.redirect(enc.url);
           }
         });
-     } else {
-       res.send("Parece que no existe esa opción. Algo salió muy mal.");
-     }
-  }
-})
+      } else {
+        res.send("Parece que no existe esa opción. Algo salió muy mal.");
+      }
+    }
+  });
 };
 
 exports.obtenerOpcionesAPI = function(req, res, next) {
   Encuesta.findOne({pregunta: req.params.pregunta}, function(err, encuesta) {
-   if (err) {
-     //console.log(err);
-     //res.send(err);
-     return next(err);
-   } else {
-       if (encuesta){
-         //console.log('FINDONE: ' + encuesta);
-         res.json(encuesta.opciones);
-       } else {
-         res.json({'msg': 'No existe la encuesta'});
-       }
-   }
-});
+    if (err) {
+      // console.log(err);
+      // res.send(err);
+      return next(err);
+    } else {
+      if (encuesta){
+        // console.log('FINDONE: ' + encuesta);
+        res.json(encuesta.opciones);
+      } else {
+        res.json({'msg': 'No existe la encuesta'});
+      }
+    }
+  });
 }
 
 // Revisar
 exports.borrarEncuesta = function (req, res, next) {
-  //busco si existe una encuesta con la pregunta dada y el usuario logueado
+  // busco si existe una encuesta con la pregunta dada y el usuario logueado
   // en caso de que exista, remove
   let pregunta = req.params.pregunta;
   Encuesta.remove({pregunta: pregunta, creador: req.user.local.username}, function(err) {
-      if (err) {
-        return next(err);
-      }
-      console.log("ENCUESTA BORRADA")
-      // No muestra el mensaje flash...
-      req.flash('success', 'Encuesta borrada.');
-      res.redirect('/');
+    if (err) {
+      return next(err);
+    }
+    console.log("ENCUESTA BORRADA")
+    // No muestra el mensaje flash...
+    req.flash('success', 'Encuesta borrada.');
+    res.redirect('/');
   });
 };
 
@@ -145,13 +144,13 @@ exports.borrarEncuesta = function (req, res, next) {
 let nuevaEncuesta = function (data) {
   console.log("Nueva encuesta.")
   let preg = data.pregunta;
-  //lo siguiente ponerlo aparte en una función auxiliar que le saque los signos de interrogación
-  preg = (preg.charAt(0) === '¿') ? preg.slice(1,preg.length) : preg;
-  preg = (preg.charAt(preg.length - 1) ==='?') ? preg.slice(0, preg.length - 1) : preg;
+  // lo siguiente ponerlo aparte en una función auxiliar que le saque los signos de interrogación
+  preg = (preg.charAt(0) === '¿') ? preg.slice(1, preg.length) : preg;
+  preg = (preg.charAt(preg.length - 1) === '?') ? preg.slice(0, preg.length - 1) : preg;
 
   let opciones = [];
 
-  //es importante el let
+  // es importante el let
   for (let item in data) {
     if (item !== 'pregunta') {
       opciones.push({ op : data[item], votos : 0 })
@@ -165,11 +164,11 @@ let nuevaEncuesta = function (data) {
 };
 
 function indiceDe(elem, arrDeObj){
-    var indice = 0;
-    for (let i = 0; i < arrDeObj.length; i++){
-      if (arrDeObj[i].op === elem){
-        indice = i;
-      }
+  var indice = 0;
+  for (let i = 0; i < arrDeObj.length; i++){
+    if (arrDeObj[i].op === elem){
+      indice = i;
     }
-    return indice;
+  }
+  return indice;
 }
