af704101ef418e10848540d9f6627c73b4cfa2a8
Flor <florenciaz@gmail.com>
2018-01-22 18:27:40 -0300

    Configuración de reglas de eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "standard",
+    "rules": {
+    	 "semi": ["off", "always"],
+    	 "space-before-function-paren": ["off"],
+    	 "camelcase": ["off"],
+       "block-spacing": ["off"],
+       "space-before-blocks": ["off"]
+
+    }
+};
