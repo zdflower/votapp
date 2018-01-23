299dd66d037f00eed99a723126d93e2da7c8c4c0
Flor <florenciaz@gmail.com>
2018-01-23 15:16:34 -0300

    index.js: isLoggedIn, comentarios y console.log para rastrear el bug al crearEncuesta.

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ const ControllerUsuarios = require('../controllers/controller_usuarios.js');
 module.exports = function(app, passport){
 // REVISAR Y REESCRIBIR ESTA FUNCIÓN:
   function isLoggedIn(req, res, next) {
+    console.log("isLoggedIn");
+    console.log('req.params: ');
+    console.log(req.params);
     if (req.isAuthenticated()) {
       if (req.user.local.username === req.params.username){
         console.log("Los usuarios coinciden.")
@@ -15,19 +18,21 @@ module.exports = function(app, passport){
         // console.log(req.params);
         return next();
       } else {
-        // req.flash('warning', 'Los usuarios no coinciden. Vuelva a su propio perfil.');
-        console.log("Los usuarios NO coinciden.")
+        // El usuario está logueado pero quiere ir a una página de otro usuario para la que se necesita autenticación, como por ejemplo el perfil, crear encuesta, borrar encuesta, etc.
+        console.log("Intenta acceder a una página de otro usuario.");
         // console.log("Usuario registrado: " + req.user.local.username);
         // console.log("Parámetro usuario: " + req.params.username);
         // console.log("Params:");
         // console.log(req.params);
-        // res.send("El usuario logueado y el de la página a la que quiere acceder no coinciden.");
         /*
           const error = new Error("El usuario logueado y el de la página a la que quiere acceder no coinciden.");
           error.status = 404;
           next(error);
         */
-        return next();
+        // return next(); //Acá me parece que hay un problema porque si los usuarios no coinciden debería devolverse un error
+        req.flash("warning", 'No puede acceder a esa página.');
+        // Lo redirijo a su propia página.
+        res.redirect(req.user.url);
       }
     } else {
       req.flash("warning", 'Si ya está registrado, debe loguearse. Si no está registrado, por favor regístrese y luego ingrese mediante login.');
