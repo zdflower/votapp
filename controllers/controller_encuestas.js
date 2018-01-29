ad3d2ae28902e818041894f8e40a700911a10285
Flor <florenciaz@gmail.com>
2018-01-29 12:35:01 -0300

    Varias correcciones a crearEncuesta_post. No se muestra un mensaje de error en la pantalla cuando el usuario envía el formulario vacío.

--- controllers/controller_encuestas.js
+++ controllers/controller_encuestas.js
@@ -3,9 +3,11 @@ const debug = require('debug')('crearEncuesta'); // ¿y si querés debuguear otr
 
 const Joi = require('joi');
 // https://www.npmjs.com/package/joi
+
+// Ojo que así como está permite que la pregunta sea "             " (puro espacio) y sospecho que pasa lo mismo con las opciones.
 const schema = Joi.object().keys({
   "pregunta": Joi.string().min(2).max(100).required(),
-  "opciones[]": Joi.array().items(Joi.string().min(2)).min(2).required()
+  "opciones": Joi.array().items(Joi.string().min(2)).min(2).required()
 })
 
 const Encuesta = require('../models/encuesta.js');
@@ -74,25 +76,46 @@ Casos de error:
       2.2) la pregunta es repetida
 */
 
+/* Por enésima vez revisar y reescribir esto */
 exports.crearEncuesta_post = function (req, res, next) {
     debug("CREA ENCUESTA.");
-    debug("Validación.");
-    /* Validación de los datos */
-    const result = Joi.validate({ pregunta: req.body.pregunta, "opciones[]": req.body["opciones[]"] }, schema, {abortEarly: false});
-    if (result.error){
-        debug('Hubo errores de validación:');
-        debug(result.error);
+    // para validar obtengo una lista de las opciones
+    if (formularioCrearEncuestaError(req, res, next)){
         // ¿Cómo uso {error: result.error} del lado del cliente, para mostrar el mensaje?
         // ¿Qué pasa si uso return next(result.error)?
-        return res.status(422).json({ error: result.error });
-      }
-      else {
-        // ¿Debería usar result.value.pregunta y result.value[opciones[]]?
-        // Los datos son válidos pero falta ver si la pregunta está repetida
+        // return res.status(422).json({ error: result.error });
+        // return next(result.error);
+        req.flash("Errores de validación."); // No muestra este mensaje.
+        res.redirect(req.user.url + '/crearEncuesta');
+    }
+    else {
        // Chequeo si ya existe una encuesta del mismo usuario con la misma pregunta
-        chequearEncuesta(req, res, next);
-    } // fin else no hubo error de validación
+       chequearEncuesta(req, res, next, datosEncuesta);
+    }
 }; // fin crearEncuesta_post
+
+function formularioCrearEncuestaError(req, res, next){
+  let opcionesKeys = Object.keys(req.body).filter(function(opc){
+    return (opc !== 'pregunta')
+  });
+  debug("opcionesKeys:");
+  debug(opcionesKeys);
+  // Quiero construir un array con req.body[key], donde key está en opcionesKeys
+  // Limpio de espacios iniciales y finales, mediante trim() tanto la pregunta como las opciones
+  let opciones = opcionesKeys.map(function(key){
+    return req.body[key].trim();
+  })
+  let datosEncuesta = {
+    pregunta: descartarSignosInterrogacion(req.body.pregunta).trim(),
+    opciones: opciones
+  };
+  /* Validación de los datos */
+  debug("Validación.");
+  const result = Joi.validate(datosEncuesta, schema, {abortEarly: false});
+  debug('result.error:');
+  debug(result.error);
+  return result.error;
+}
 
 exports.votarEncuesta = function(req, res, next) {
   const filtro = {'pregunta': req.params.pregunta, 'creador': req.params.username};
@@ -154,46 +177,41 @@ exports.borrarEncuesta = function (req, res, next) {
   });
 };
 
-
-function chequearEncuesta(req, res, next){
-  let usuario_logueado = req.user
-  let preg = descartarSignosInterrogacion(req.body.pregunta);
+// datosEncuesta tiene la forma { pregunta: String, opciones: Array of String}
+function chequearEncuesta(req, res, next, datosEncuesta){
+  let usuario_logueado = req.user;
+  let preg = datosEncuesta.pregunta; // Ya está sin signos de interrogación y sin espacios iniciales ni finales.
   debug('Usuario: ' + usuario_logueado.local.username + '\nPregunta: ' + preg);
   Encuesta.findOne({pregunta: preg, creador: usuario_logueado.local.username}, function chequearEncRepetida(err, resultado){
     if (err){
       debug("Error: " + err);
-      req.flash('error', 'Algo salió mal al buscar la encuesta.');
-      res.send(err);
+      return next(err);
     }
     // No hubo error en la búsqueda.
     debug('Resultado de la búsqueda de una encuesta repetida: ' + resultado);
     if (resultado) {
       debug('ERROR: Ya existe una encuesta con la pregunta: "' + preg + '"');
-      req.flash('error', 'Ya existe una encuesta con la pregunta: "' + preg + '"');
-      const error = new Error('Ya existe una encuesta con la pregunta: "' + preg + '"');
-      error.status = 400;
-      // quizá en vez de err sea error... Sí, eso lo solucionó.
-      return next(error);//res.send(error);
+      req.flash('error', 'Ya existe una encuesta con la pregunta: "' + preg + '"'); // Este sí lo muestra.
+      // const error = new Error('Ya existe una encuesta con la pregunta: "' + preg + '"');
+      // error.status = 400;
+      //return next(error);
+      res.redirect(usuario_logueado.url + '/crearEncuesta');
     } else {
       debug('Aparentemente NO habría una encuesta del mismo usuario con esa pregunta');
       // Llegado acá, se cumplen todas las condiciones para crear la encuesta.
       // Creo la nueva encuesta y la guardo.
-      // Acá está el problema de los signos de interrogación: como paso req.body, no usa la pregunta sin signos de interrogación, sino la original.
-      // Le paso además del body la pregunta sin los signos de interrogación, por lo menos hasta que sepa si se puede (y cómo) modificar req.body.pregunta y así pasarle sólo req.body, o hasta que encuentre una solución mejor.
-      let nueva_encuesta = new Encuesta(nuevaEncuesta(preg, req.body));
-      // ¿Cómo se manejan posibles errores en nuevaEncuesta() y en new Encuesta()?
+      let nueva_encuesta = new Encuesta(nuevaEncuesta(datosEncuesta));
       nueva_encuesta.creador = usuario_logueado.local.username;
       debug("nueva_encuesta: ");
       debug(nueva_encuesta);
       nueva_encuesta.save(function (err) {
         if (err) {
           debug("Error guardando encuesta.");
-          req.flash('error', 'Algo salió mal al intentar guardar la encuesta.');
-          res.send(err);
+          return next(err);
         }
         // si no hubo error llegamos acá, si hubo error, creo que no llegás acá.
         req.flash('success', 'Encuesta creada');
-        res.send('Ok');
+        res.redirect(usuario_logueado.url);
       }); // fin guardado de encuesta
     } // fin else no hay encuesta repetida
   }); // fin búsqueda encuesta
@@ -207,17 +225,17 @@ function descartarSignosInterrogacion(pregunta) {
   return preg;
 }
 
-// data.pregunta es la original,
-// pregunta es la que no tiene signos de interrogación
-// data.cantidadOpciones es la cantidad de opciones,
-// data.opX (donde X es un número de 1 en adelante) es el nombre de la opción.
-let nuevaEncuesta = function (pregunta, data) {
+// data.pregunta no tiene signos de interrogación ni espacios al comienzo ni al final,
+// data.opciones es un array con las opciones.
+function nuevaEncuesta(data) {
   debug("Nueva encuesta.");
-  let opciones = data["opciones[]"].map(function(opcion){
-      return {op : opcion, votos : 0};
+  let opciones = data.opciones.map(function(opcion){
+    return {op : opcion, votos : 0};
   });
+  debug('opciones:');
+  debug(opciones);
   return {
-    pregunta : pregunta,
+    pregunta : data.pregunta,
     opciones : opciones
   };
 };
