47d2f5a1c8cc9d95411f97820dd4f1c257a6886c
Flor <florenciaz@gmail.com>
2018-01-30 17:50:53 -0300

    Chequeo que la nueva opción no sea una cadena vacía y que por lo menos tenga dos caracteres. Borro los mensajes de error previos después de clickear en agregar opción.

--- public/js/votaNuevaOp.js
+++ public/js/votaNuevaOp.js
@@ -2,23 +2,27 @@ function esValida(opcion){
   // opción debe ser una cadena de caracteres no vacía de más de 1 carácter y no tener espacios extra
   // sería bueno que no hubiera otra opción igual a la que se quiere agregar.
   // Completar:
-  return opcion.trim() !== '';
+  const novacia = opcion.trim() !== '';
+  const alMenosDosChar = opcion.trim().length >= 2;
+  let result =  novacia && alMenosDosChar
+  return result;
 }
 
 $(document).ready(function(){
   $('#otraOpcion').click(function(){
+    // De haber un aviso de error, se borra.
+    $('#errorVota').empty();
     let nuevaOpcion = $('input[name="otraOp"]').val();
     if (esValida(nuevaOpcion)){
-      // Sacar cualquier mensaje de error que hubiera habido.
-      $('#errorVota').empty();
-      $('#votarOpciones').append(
+        $('#votarOpciones').append(
         '<div class="form-check">'
           + '<input class="form-check-input" name="op" type="radio" value="' + nuevaOpcion + '">'
           + '<label class="form-check-label">' + nuevaOpcion + '</label>'
         + '</div>'
       );
     } else {
-      /* mostrar un mensaje de error */
+      // Borro el mensaje anterior, si lo hubiera
+      /* Mostrar un mensaje de error */
       $('#errorVota').append(
       '<div class="alert alert-dismissable alert-warning">'
       + '<button type="button" class="close" data-dismiss="alert">&times</button>'
